8cae9f5a32b889caf4c6d827ffd585978b9a3582
Ростислав <espaddda@gmail.com>
2024-11-02 09:17:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import flet as ft
+from router import Router
+
+
+def main(page: ft.page):
+    Router(page)
+
+
+if __name__ == "__main__":
+    ft.app(target=main, assets_dir="assets")
---
+++ router.py
@@ -0,0 +1,19 @@
+import flet as ft
+from flet_route import Routing, path
+from pages.login import LoginPage
+from pages.signup import SignupPage
+
+
+class Router:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.app_routes = [
+            path(url='/', clear=True, view=LoginPage().view),
+            path(url='/signup', clear=False, view=SignupPage().view)
+        ]
+
+        Routing(
+            page=self.page,
+            app_routes=self.app_routes,
+        )
+        self.page.go(self.page.route)
