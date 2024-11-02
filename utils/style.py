884bdf2afdd115a7c85375f01b8c0a587a22d947
Ростислав <espaddda@gmail.com>
2024-11-02 09:19:55 +0300

    Add files via upload

---
+++ utils/style.py
@@ -0,0 +1,11 @@
+#Стили
+defaultBgColor = '#000000'
+defaultFontColor = '#ffffff'
+secondaryBgColor = '#121222'
+secondaryFontColor = '#9b9c9c'
+inputBgColor = '#1a1a2a'
+hoverBgColor = '#0068f7'
+#Настройки окна
+defaultWidthWindow = '1000'
+defaultHeightWindow = '600'
+inputBgErrorColor = '#FF0000'
